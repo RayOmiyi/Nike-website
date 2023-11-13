7fe7e9f293fd142e270fa4716eddb3d7e55cfb38
RayOmiyi <rayikponmwonsaomiyi@gmail.com>
2023-11-13 18:19:07 +0100

    first commit

---
+++ src/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AiFillStar } from 'react-icons/ai';
+import {BsFillBagHeartFill} from 'react-icons/bs'
+import './Products.css'
+const Products = ({ result }) => {
+  return (
+    <>
+      <section className="card-container">{result}</section>
+    </>
+  );
+};
+
+export default Products
